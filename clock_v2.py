dee00dcfb704808cd249f5df2e1f18cd75a6ebbb
basti-ang <bastian.angerer@outlook.com>
2024-01-19 10:47:18 +0100

    update v2

--- clock_v2.py
+++ clock_v2.py
@@ -1,25 +1,33 @@
-import requests
 import time
 from datetime import datetime
+import requests
 
-#Controller Address 
+def wledClock():
+    url = "****"
+    current_time = datetime.now().strftime("%H:%M")
+    data = {"seg": [{"id": 0, "n": current_time}]}
 
-url = "http://IP.GOES.HERE/json/state"
+    try:
+        response = requests.post(url, json=data)
+    except requests.RequestException as e:
+        print(f"Fehler beim Senden der Uhrzeit: {e}")
 
-#first update on startup
-
-current_time = datetime.now().strftime("%H:%M")
-data = {"seg": [{"id": 0, "n": current_time}]}
-response = requests.post(url, json=data)
-
-#update every full minute
-
-while True:
     now = datetime.now()
     if now.second == 0:
         current_time = datetime.now().strftime("%H:%M")
         data = {"seg": [{"id": 0, "n": current_time}]}
-        response = requests.post(url, json=data)
 
-    time.sleep((60 - now.second) % 60)
+        try:
+            response = requests.post(url, json=data)
+        except requests.RequestException as e:
+            print(f"Fehler beim erneuten Senden der Uhrzeit: {e}")
 
+        time.sleep((60 - now.second) % 60)
+
+while True:
+    try:
+        wledClock()
+    except Exception as e:
+        print(f"Fehler aufgetreten: {e}")
+
+    time.sleep(180) 
